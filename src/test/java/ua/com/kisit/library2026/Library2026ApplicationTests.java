f29cc0d33d05edaa4d5286cd19b0e147e192f72b
mariiakuzmenko <mariiakuzmenko.mk7@gmail.com>
2026-02-09 13:41:06 +0100

    project setup. lr2

---
+++ src/test/java/ua/com/kisit/library2026/Library2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.kisit.library2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Library2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
